ed679c706b4c633be0f49869cc5bd7647a2e4e54
DomamaN202 <vip.domaman@mail.ru>
2023-12-10 12:05:04 +0700

    =)

--- src/main/kotlin/ru/DmN/siberia/scs/compiler/json/compilers/NCMap.kt
+++ src/main/kotlin/ru/DmN/siberia/scs/compiler/json/compilers/NCMap.kt
@@ -3,6 +3,7 @@ package ru.DmN.phtx.pcl.compiler.json.compilers
 import ru.DmN.siberia.scs.ast.NodeLazyMap
 import ru.DmN.phtx.pcl.compiler.json.utils.indent
 import ru.DmN.phtx.pcl.compiler.json.utils.out
+import ru.DmN.phtx.pcl.compiler.json.utils.with
 import ru.DmN.siberia.Compiler
 import ru.DmN.siberia.compiler.ctx.CompilationContext
 import ru.DmN.siberia.compilers.INodeCompiler
@@ -17,10 +18,10 @@ object NCMap : INodeCompiler<NodeLazyMap> {
             var i = 0
             node.forEach {
                 out.append('\n').append("\t".repeat(indent + 1))
-                compiler.compile(it, ctx)
+                compiler.compile(it, ctx.with(indent + 1))
                 if (++i != node.size)
                     out.append(',')
-                else out.append("\n\t")
+                else out.append('\n').append("\t".repeat(indent))
             }
         }
         out.append('}')
--- src/main/kotlin/ru/DmN/siberia/scs/compiler/json/utils/Utils.kt
+++ src/main/kotlin/ru/DmN/siberia/scs/compiler/json/utils/Utils.kt
@@ -2,6 +2,9 @@ package ru.DmN.phtx.pcl.compiler.json.utils
 
 import ru.DmN.siberia.utils.IContextCollection
 
+fun <T : IContextCollection<T>> IContextCollection<T>.with(indent: Int) =
+    this.with("phtx/pcl/indent", indent)
+
 var <T : IContextCollection<T>> IContextCollection<T>.out: StringBuilder
     set(value) { this.contexts["phtx/pcl/out"] = value }
     get() = this.contexts["phtx/pcl/out"] as StringBuilder
